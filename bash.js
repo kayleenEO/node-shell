ca18e8e2478de78c82322cd5a2c0e1a7e45c44a5
Brendan Meyer <brenmeyer2245@gmail.com>
2018-06-12 11:23:22 -0500

    NoDemon

---
+++ bash.js
@@ -0,0 +1,19 @@
+process.stdout.write('prompt > ');
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+
+  if (cmd === "pwd") pwd()
+  else {
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+  }
+
+
+
+});
+
+
+function pwd(){
+
+  console.log(`Current directory: ${process.cwd()}`);
+}
